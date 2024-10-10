bf702193c40058220538252025f6e0eacd6bdc04
Humoyunmirzo Xoliqberdiyev <humoyunmirzo800@gmail.com>
2024-10-10 14:02:04 +0500

    changed PORT

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ async def remove_bg(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 4000)))
