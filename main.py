3f258d3f57a36ab5e1c9978cab440805dd9d34cb
Humoyunmirzo Xoliqberdiyev <humoyunmirzo800@gmail.com>
2024-10-28 10:45:25 +0500

    message

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ import os
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to my API!"}
-
 @app.post("/image")
 async def remove_bg(file: UploadFile = File(...)):
     # Faylni o'qish
@@ -22,9 +18,15 @@ async def remove_bg(file: UploadFile = File(...)):
     # Fonni o'chirish
     output_image = remove(input_image)
 
+    # Hajmini kamaytirish (masalan, 80% sifatida)
+    output_image = output_image.resize(
+        (int(output_image.width * 0.8), int(output_image.height * 0.8)),
+        Image.LANCZOS  # ANTIALIAS o'rniga LANCZOS ishlatiladi
+    )
+
     # Yangi rasmni saqlash uchun io.BytesIO dan foydalanamiz
     output_io = io.BytesIO()
-    output_image.save(output_io, format='PNG')
+    output_image.save(output_io, format='PNG', optimize=True, quality=90)
     output_io.seek(0)  # Faylni boshidan o'qish uchun
 
     # Faylni StreamingResponse yordamida qaytarish
